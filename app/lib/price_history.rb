9d9bc8019230532eacc55ce2f6b2b02ee9debc97
CarlChaappletree <carlcha.appletree@gmail.com>
2022-06-07 12:54:46 +0200

    Update app/lib/price_history.rb with spec

--- app/lib/price_history.rb
+++ app/lib/price_history.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 class PriceHistory
-  def self.call(...)
-
+  def self.call(package:, year:, **options)
     # Feature request 2: Pricing History
     #
     # NOTE: Some tests for this file have already been written, but feel free to add
     # more and change the old tests.
-
-    raise NotImplementedError, "Implement this to pass the assignment"
+    obj = {}
+    locations = package.locations
+    locations = locations.where(municipality: options[:municipality]) if options[:municipality].present?
+    locations.each do |l|
+      l.price_logs.where("cast(strftime('%Y', created_at) as int) = ?", year).each do |p|
+        if obj[l.municipality]
+          obj[l.municipality] << p.price_cents
+        else
+          obj[l.municipality] = [p.price_cents]
+        end
+      end
+    end
+    obj
   end
 end
--- spec/lib/price_history_spec.rb
+++ spec/lib/price_history_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PriceHistory do
   # These tests cover feature request 2. Feel free to add more tests or change
   # the existing ones.
 
-  xit "returns the pricing history for the provided year and package" do
+  it "returns the pricing history for the provided year and package" do
     basic = Package.create!(name: "basic")
 
     travel_to Time.zone.local(2019) do
@@ -24,12 +24,18 @@ RSpec.describe PriceHistory do
     history = PriceHistory.call(package: basic, year: "2020")
     expect(history).to eq(
       "Göteborg" => [100_00],
-      "Stockholm" => [30_00, 40_00],
+      "Stockholm" => [30_00, 40_00]
     )
   end
 
-  xit "supports filtering on municipality" do
+  it "supports filtering on municipality" do
     basic = Package.create!(name: "basic")
+
+    travel_to Time.zone.local(2019) do
+      UpdatePackagePrice.call(basic, 30_00, municipality: "Stockholm")
+      UpdatePackagePrice.call(basic, 40_00, municipality: "Stockholm")
+      UpdatePackagePrice.call(basic, 100_00, municipality: "Göteborg")
+    end
 
     travel_to Time.zone.local(2020) do
       UpdatePackagePrice.call(basic, 30_00, municipality: "Stockholm")
@@ -37,6 +43,9 @@ RSpec.describe PriceHistory do
       UpdatePackagePrice.call(basic, 100_00, municipality: "Göteborg")
     end
 
-    # Whoops no assertions, please add some
+    history = PriceHistory.call(package: basic, year: "2020", municipality: "Stockholm")
+    expect(history).to eq(
+      "Stockholm" => [30_00, 40_00]
+    )
   end
 end
