5ab44191e368a5840ea4b70b216dacb72a8194c7
CarlChaappletree <carlcha.appletree@gmail.com>
2022-06-07 12:12:53 +0200

    Update app/services/update_package_price.rb with spec

--- app/services/update_package_price.rb
+++ app/services/update_package_price.rb
@@ -3,10 +3,19 @@
 class UpdatePackagePrice
   def self.call(package, new_price_cents, **options)
     Package.transaction do
-      # Add a pricing history record
-      Price.create!(package: package, price_cents: package.price_cents)
+      location = Location.find_or_create_by(municipality: options[:municipality])
+      price_with_location = package.prices.find_by(location: location)
+      # Add a price depending on a municipality
+      if price_with_location.present?
+        price_with_location.update!(price_cents: new_price_cents)
+      else
+        # TODO: After defining the package.price_cents, price.location should not allow null
+        Price.create!(package: package, price_cents: new_price_cents, location: location)
+      end
 
+      PriceLog.create!(package: package, price_cents: new_price_cents, default_price_cents: package.price_cents, location: location)
       # Update the current price
+      # TODO: What municipality package.price_cents represent?
       Package.update!(price_cents: new_price_cents)
     end
   end
